dae1bfefc301f7f2b7d7e82e168cc897eb8ef218
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-07-06 12:39:21 +0900

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,25 @@ import { Typography } from '@mui/material'
 
 export const Header = () => {
   return (
-    <Typography
-      component="h1"
-      variant="h4"
-      align="center"
-      sx={{ my: 4, fontWeight: 300 }}
-    >
-      The night is still young
-    </Typography>
+    <>
+      <Typography
+        component="h1"
+        variant="h4"
+        align="center"
+        sx={{ mt: 4, fontWeight: 300 }}
+      >
+        東京は夜の七時
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        align="center"
+        sx={{ mb: 4, fontWeight: 300 }}
+      >
+        The night is still young
+      </Typography>
+      <Typography variant="body1" align="center" sx={{ mb: 4 }}>
+        タイムゾーンをまたいだ時間の確認が簡単にできるWebサイトです。
+      </Typography>
+    </>
   )
 }
